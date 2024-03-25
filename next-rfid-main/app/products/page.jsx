903f643e145767eb5449bc423befd4f94c0e4c06
Zhanybek Bekbolat <daveelfie@gmail.com>
2024-03-25 16:57:12 +0500

    Connect to RTDB and adjust the display

--- next-rfid-main/app/products/page.jsx
+++ next-rfid-main/app/products/page.jsx
@@ -8,6 +8,9 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@firebase/config";
+
+// For Firestore:
+/*
 import {
   collection,
   addDoc,
@@ -17,16 +20,21 @@ import {
   query,
 } from "firebase/firestore";
 import { db } from "@firebase/config";
-import { rtdb } from "@firebase/config";
+*/
+
+// For RTDB:
+import { get, ref } from 'firebase/database';
+import { rtdb } from '@firebase/config';
 
 const Products = () => {
   const [items, setItems] = useState([]);
   const [userSession, setUserSession] = useState(null);
   const [user] = useAuthState(auth);
 
+  // Firestore code:
+  /*
   useEffect(() => {
-    setUserSession(sessionStorage.getItem("user"));
-    const q = query(collection(db, "products"));
+    const q = query(collection(db, "feed"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let itemsArr = [];
 
@@ -35,10 +43,30 @@ const Products = () => {
       });
       setItems(itemsArr);
     });
+  }, []); 
+  */
+
+  // RTDB code:
+  useEffect(() => {
+    const itemsRef = ref(rtdb, 'test');
+    get(itemsRef).then((snapshot) => {
+      if(snapshot.exists()) {
+        const itemsArr = Object.entries(snapshot.val()).map(([id, data]) => ({
+          id,
+          ...data,
+        }));
+        setItems(itemsArr);
+      }
+      else {
+        console.log("No data available...");
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
   }, []);
 
   const transformString = (str) => {
-    return str.toLowerCase().replace(/\s+/g, '_');
+    return str.replace(/_/g, ' ').replace(/^\w/, c => c.toUpperCase());
   }
 
   return (
@@ -48,11 +76,12 @@ const Products = () => {
         <section className="prompt_layout">
           {items.map((item) => (
             <div className="flex justify-between items-start gap-5">
-            <div key={item.id} className="prompt_card">
-              <Image className="object-contain h-[300px] w-[300px] rounded-lg" src={`/assets/images/${transformString(item.type)}.jpeg`} alt="Image of concrete" width={300} height={200} />
-              <h2 className="green_sm_head_text">{item.id}</h2>
-              <p className="prod_desc">Price: ${item.price}</p>
-              <p className="prod_desc">Type: {item.type}</p>
+            <div key={item.id} className="item_card">
+              <Image className="object-contain h-[300px] w-[300px] rounded-lg" src={`/assets/images/${item.type}.jpeg`} alt="Image of concrete" width={300} height={200} />
+              <h2 className="green_smst_head_text">{item.id}</h2>
+              <p className="prod_smst_desc">Price: ${item.price}</p>
+              <p className="prod_smst_desc">Type: {transformString(item.type)}</p>
+              <p className="prod_smst_desc">Last Location: {item.location}</p>
               <Link href="/" className="edit_btn mt-5">
                 Edit
               </Link>
--- next-rfid-main/components/Feed.jsx
+++ next-rfid-main/components/Feed.jsx
@@ -6,6 +6,8 @@ import Image from 'next/image';
 
 import { useState, useEffect } from "react";
 
+// For Firestore:
+/*
 import {
   collection,
   addDoc,
@@ -15,9 +17,16 @@ import {
   query,
 } from "firebase/firestore";
 import { db } from "@firebase/config";
+*/
+
+// For RTDB:
+import { get, ref } from 'firebase/database';
+import { rtdb } from '@firebase/config';
 
 const Feed = () => {
   const [items, setItems] = useState([]);
+  // Firestore code:
+  /*
   useEffect(() => {
     const q = query(collection(db, "feed"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
@@ -27,6 +36,26 @@ const Feed = () => {
         itemsArr.push({ ...doc.data(), id: doc.id });
       });
       setItems(itemsArr);
+    });
+  }, []); 
+  */
+
+  // RTDB code:
+  useEffect(() => {
+    const itemsRef = ref(rtdb, 'feed_prompts');
+    get(itemsRef).then((snapshot) => {
+      if(snapshot.exists()) {
+        const itemsArr = Object.entries(snapshot.val()).map(([id, data]) => ({
+          id,
+          ...data,
+        }));
+        setItems(itemsArr);
+      }
+      else {
+        console.log("No data available...");
+      }
+    }).catch((error) => {
+      console.error(error);
     });
   }, []);
 
@@ -39,9 +68,9 @@ const Feed = () => {
       <div className='mt-4 feed_layout'>
       {items.map((item) => (
         <div key={item.id} className="feed_card">
-              <h2 className="feed_sm_head_text">{item.id}</h2>
-              <Image className="object-contain h-[150px] w-[300px] rounded-lg" src={`/assets/images/${transformString(item.id)}.jpg`} alt="Feed image" width={300} height={200} />
-              <p className="prod_desc">{item.description}</p>
+              <h2 className="feed_sm_head_text">{item.title}</h2>
+              <Image className="object-contain h-[150px] w-[300px] rounded-lg" src={`/assets/images/${transformString(item.title)}.jpg`} alt="Feed image" width={300} height={200} />
+              <p className="prod_desc">{item.subtitle}</p>
             </div>
           ))}
       </div>
